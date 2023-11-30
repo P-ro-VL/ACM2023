8cd14b8329aa93212310bbaf3729d55de2f50892
phongphong.28 <119289183+P-ro-VL@users.noreply.github.com>
2023-12-01 01:15:29 +0700

    Update 2013_A.cpp

--- 2013/2013_A.cpp
+++ 2013/2013_A.cpp
@@ -14,17 +14,12 @@ int main()
         lli k;
         cin >> k;
 
-        if (k % 2 == 0)
-        {
-            cout << "-1 -1" << endl;
-            continue;
-        }
-
+        bool found = false;
         for (lli m = sqrt(k); m >= 1; m--)
         {
             double n = (2 * k - m - 1) / (double)(2 * m + 1);
 
-            if (floor(n) == n && n > 1)
+            if (floor(n) == n && n >= m)
             {
                 lli a = m, b = n;
                 if (a > b)
@@ -34,8 +29,14 @@ int main()
                     b = tg;
                 }
                 cout << a << " " << b << endl;
+                found = true;
                 break;
             }
+        }
+
+        if (found == false)
+        {
+            cout << "-1 -1" << endl;
         }
     }
 }
